c2722792224c4836a4e7186909a3e18e0dd50f43
guille552 <guillermo.medina021102@gmail.com>
2023-11-06 17:08:03 -0600

    integracion de interfaz

--- se copia.py
+++ se copia.py
@@ -1,5 +1,8 @@
+import tkinter as tk
+from tkinter import ttk
 import experta
 
+# Definición de hechos (facts)
 class Enfermedad(experta.Fact):
     pass
 
@@ -15,21 +18,22 @@ class Adicciones(experta.Fact):
 class Diagnostico(experta.Fact):
     pass
 
+# Definición del sistema experto
 class SistemaExperto(experta.KnowledgeEngine):
 
-    @experta.Rule(Enfermedad(sintomas='si') & Edad('adulto') & Adicciones('si'))
+    @experta.Rule(Enfermedad(sintomas='si') & Edad(lambda x: x >= 18) & Adicciones('si'))
     def regla_1(self):
         self.declare(Diagnostico(enfermedad="Intoxicación Alimentaria"))
 
-    @experta.Rule(Enfermedad(sintomas='si') & Edad('adulto') & Adicciones('no'))
+    @experta.Rule(Enfermedad(sintomas='si') & Edad(lambda x: x >= 18) & Adicciones('no'))
     def regla_2(self):
         self.declare(Diagnostico(enfermedad="Gripe"))
 
-    @experta.Rule(Enfermedad(sintomas='si') & Edad('niño') & Adicciones('si'))
+    @experta.Rule(Enfermedad(sintomas='si') & Edad(lambda x: x < 18) & Adicciones('si'))
     def regla_3(self):
         self.declare(Diagnostico(enfermedad="Resfriado Común"))
 
-    @experta.Rule(Enfermedad(sintomas='si') & Edad('niño') & Adicciones('no'))
+    @experta.Rule(Enfermedad(sintomas='si') & Edad(lambda x: x < 18) & Adicciones('no'))
     def regla_4(self):
         self.declare(Diagnostico(enfermedad="Gripe"))
 
@@ -40,25 +44,53 @@ class SistemaExperto(experta.KnowledgeEngine):
 def ejecutar_sistema_experto():
     engine = SistemaExperto()
 
-    # Preguntar por síntomas
-    respuesta_sintomas = input("¿Experimenta síntomas? (si/no): ")
+    respuesta_sintomas = cuadro_sintomas.get()
+    engine.reset()  
     engine.declare(Enfermedad(sintomas=respuesta_sintomas))
 
-    # Preguntar por edad
-    respuesta_edad = input("¿Es usted un niño o un adulto? (niño/adulto): ")
+    try:
+        respuesta_edad = int(cuadro_edad.get())
+    except ValueError:
+        cuadro_diagnostico.config(text="Por favor, ingrese una edad válida.")
+        return
+
     engine.declare(Edad(respuesta_edad))
 
-    # Preguntar por adicciones
-    respuesta_adicciones = input("¿Tiene adicciones? (si/no): ")
+    respuesta_adicciones = cuadro_adicciones.get()
     engine.declare(Adicciones(respuesta_adicciones))
 
-    # Ejecutar el sistema experto
     engine.run()
 
-    # Obtener el diagnóstico
     diagnostico = engine.facts[Diagnostico][-1]
 
-    print(f"El diagnóstico es: {diagnostico['enfermedad']}")
+    cuadro_diagnostico.config(text=f"El diagnóstico es: {diagnostico['enfermedad']}")
 
-if __name__ == "__main__":
-    ejecutar_sistema_experto()
+ventana = tk.Tk()
+ventana.title("Sistema experto médico para la detección de enfermedades")
+ventana.config(width=500, height=400)
+
+etiqueta_sintomas = ttk.Label(ventana, text="¿Experimenta síntomas? (si/no):")
+etiqueta_sintomas.pack()
+
+cuadro_sintomas = ttk.Entry(ventana)
+cuadro_sintomas.pack()
+
+etiqueta_edad = ttk.Label(ventana, text="Ingrese su edad:")
+etiqueta_edad.pack()
+
+cuadro_edad = ttk.Entry(ventana)
+cuadro_edad.pack()
+
+etiqueta_adicciones = ttk.Label(ventana, text="¿Tiene adicciones? (si/no):")
+etiqueta_adicciones.pack()
+
+cuadro_adicciones = ttk.Entry(ventana)
+cuadro_adicciones.pack()
+
+boton_consultar = ttk.Button(ventana, text="Consultar diagnóstico", command=ejecutar_sistema_experto)
+boton_consultar.pack()
+
+cuadro_diagnostico = ttk.Label(ventana, text="Bienvenido al sistema médico")
+cuadro_diagnostico.pack()
+
+ventana.mainloop()
