40c3de51f626f950770955a1d10032e6ab716cfe
guille552 <guillermo.medina021102@gmail.com>
2023-11-06 16:17:55 -0600

    ejercicio resuelto

--- se copia.py
+++ se copia.py
@@ -1,89 +1,64 @@
-import tkinter as tk
-from tkinter import ttk
 import experta
-from experta import Rule, KnowledgeBase, InferenceEngine, abc
 
-# Definir la base de conocimientos
-base_conocimientos = KnowledgeBase()
+class Enfermedad(experta.Fact):
+    pass
 
-# Definir las reglas
-class GripeRule(Rule):
-    def match(self, facts):
-        fiebre = facts["fiebre"] >= 38
-        tos = facts["tos"]
-        return fiebre and tos
+class Sintoma(experta.Fact):
+    pass
 
-class InfluenzaRule(Rule):
-    def match(self, facts):
-        fiebre = facts["fiebre"] >= 39
-        dolor_cabeza = facts["dolor_cabeza"]
-        return fiebre and dolor_cabeza
+class Edad(experta.Fact):
+    pass
 
-class NeumoniaRule(Rule):
-    def match(self, facts):
-        fiebre = facts["fiebre"] >= 38
-        dificultad_respirar = facts["dificultad_respirar"]
-        return fiebre and dificultad_respirar
+class Adicciones(experta.Fact):
+    pass
 
-class GastroenteritisRule(Rule):
-    def match(self, facts):
-        diarrea = facts["diarrea"]
-        vomito = facts[" vomito"]
-        return diarrea and vomito
+class Diagnostico(experta.Fact):
+    pass
 
-# Agregar las reglas a la base de conocimientos
-base_conocimientos.add_rule(GripeRule())
-base_conocimientos.add_rule(InfluenzaRule())
-base_conocimientos.add_rule(NeumoniaRule())
-base_conocimientos.add_rule(GastroenteritisRule())
+class SistemaExperto(experta.KnowledgeEngine):
 
-# Crear la interfaz gráfica de usuario
-ventana = tk.Tk()
-ventana.title("Sistema experto médico para la detección de enfermedades")
-ventana.config(width=500, height=400)
+    @experta.Rule(Enfermedad(sintomas='si') & Edad('adulto') & Adicciones('si'))
+    def regla_1(self):
+        self.declare(Diagnostico(enfermedad="Intoxicación Alimentaria"))
 
-# Agregar un cuadro de entrada para los hechos
-cuadro_entrada = ttk.Entry(ventana)
-cuadro_entrada.pack()
+    @experta.Rule(Enfermedad(sintomas='si') & Edad('adulto') & Adicciones('no'))
+    def regla_2(self):
+        self.declare(Diagnostico(enfermedad="Gripe"))
 
-# Agregar un botón de guardar en la base de hechos
-boton_guardar = ttk.Button(ventana, text="Guardar en la base de hechos")
-boton_guardar.pack()
+    @experta.Rule(Enfermedad(sintomas='si') & Edad('niño') & Adicciones('si'))
+    def regla_3(self):
+        self.declare(Diagnostico(enfermedad="Resfriado Común"))
 
-# Agregar un botón de consultar el diagnóstico
-boton_consultar = ttk.Button(ventana, text="Consultar diagnóstico")
-boton_consultar.pack()
+    @experta.Rule(Enfermedad(sintomas='si') & Edad('niño') & Adicciones('no'))
+    def regla_4(self):
+        self.declare(Diagnostico(enfermedad="Gripe"))
 
-# Agregar un cuadro de texto para mostrar el diagnóstico
-cuadro_diagnostico = ttk.Label(ventana, text="Bienvenido al sistema experto médico")
-cuadro_diagnostico.pack()
+    @experta.Rule(Enfermedad(sintomas='no'))
+    def regla_5(self):
+        self.declare(Diagnostico(enfermedad="Sin enfermedad identificada"))
 
-# Agregar el evento al botón de guardar en la base de hechos
-boton_guardar.config(command=lambda: guardar_hechos(cuadro_entrada.get()))
+def ejecutar_sistema_experto():
+    engine = SistemaExperto()
 
-# Agregar el evento al botón de consultar el diagnóstico
-boton_consultar.config(command=lambda: consultar_diagnostico())
+    # Preguntar por síntomas
+    respuesta_sintomas = input("¿Experimenta síntomas? (si/no): ")
+    engine.declare(Enfermedad(sintomas=respuesta_sintomas))
 
-# Función para guardar los hechos en la base de conocimientos
-def guardar_hechos(hechos):
-    base_conocimientos.update({hechos: True})
-    print(f"Los hechos {hechos} se han guardado en la base de conocimientos.")
+    # Preguntar por edad
+    respuesta_edad = input("¿Es usted un niño o un adulto? (niño/adulto): ")
+    engine.declare(Edad(respuesta_edad))
 
-# Función para consultar el diagnóstico
-def consultar_diagnostico():
-    hechos = cuadro_entrada.get()
-    diagnostico = inferir_diagnostico(hechos)
-    if diagnostico is None:
-        diagnostico = "No se puede determinar el diagnóstico"
-    cuadro_diagnostico.config(text=f"El diagnóstico es: {diagnostico}")
+    # Preguntar por adicciones
+    respuesta_adicciones = input("¿Tiene adicciones? (si/no): ")
+    engine.declare(Adicciones(respuesta_adicciones))
 
-# Función para inferir el diagnóstico
-def inferir_diagnostico(hechos):
-    for regla, conclusion in base_conocimientos.items():
-        coincidencias = [hecho for hecho in hechos if hecho in regla.split("y")]
-        if all(coincidencias):
-            return conclusion
-    return None
+    # Ejecutar el sistema experto
+    engine.run()
 
-# Iniciar la interfaz gráfica de usuario
-ventana.mainloop()
+    # Obtener el diagnóstico
+    diagnostico = engine.facts[Diagnostico][-1]
+
+    print(f"El diagnóstico es: {diagnostico['enfermedad']}")
+
+if __name__ == "__main__":
+    ejecutar_sistema_experto()
