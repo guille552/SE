192f44f794c15f3f48978936ba93858e3d78d036
guille552 <guillermo.medina021102@gmail.com>
2023-11-09 16:43:38 -0600

    nueva ventana

--- intento 4.py
+++ intento 4.py
@@ -1,82 +1,153 @@
 import tkinter as tk
 from tkinter import ttk
 
-# Base de Hechos (Fact Base)
 base_de_hechos = {}
 
-# Lista de Factores de Riesgo con Síntomas
+cuadro_fiebre = None
+cuadro_tos = None
+cuadro_dolor_cabeza = None
+cuadro_dificultad_respirar = None
+cuadro_diarrea = None
+cuadro_vomito = None
+combo_factores_riesgo = None
+combo_factores_riesgo2 = None
+combo_adicciones = None
+combo_edad = None
+cuadro_diagnostico = None
+
 factores_riesgo = {
-    "Ninguno":"",
+    "Ninguno": "",
     "Obesidad": "Aumento de peso, Dificultad para moverse",
     "Anorexia": "Pérdida de peso, Debilidad"
-    
 }
 
-# Lista de Adicciones
 adicciones = ["Ninguno", "Alcoholismo", "Tabaquismo", "Drogadicción"]
 
-# Función para guardar factores de riesgo y otros datos en la base de hechos
 def guardar_factores_riesgo():
-    # Se guarda cada síntoma y su valor en la base de hechos
-    base_de_hechos["fiebre"] = cuadro_fiebre.get().lower() == "si"
-    base_de_hechos["tos"] = cuadro_tos.get().lower() == "si"
-    base_de_hechos["dolor_cabeza"] = cuadro_dolor_cabeza.get().lower() == "si"
-    base_de_hechos["dificultad_respirar"] = cuadro_dificultad_respirar.get().lower() == "si"
-    base_de_hechos["diarrea"] = cuadro_diarrea.get().lower() == "si"
-    base_de_hechos["vomito"] = cuadro_vomito.get().lower() == "si"
-    
-    # Se obtienen los factores de riesgo seleccionados
-    factores_seleccionados = combo_factores_riesgo.get()
-    base_de_hechos["factores_riesgo"] = factores_seleccionados
-    
-    # Se deshabilita Anorexia y Obesidad en el otro combo box
-    if "Anorexia" in factores_seleccionados:
+    base_de_hechos["fiebre"] = cuadro_fiebre.instate(['selected'])
+    base_de_hechos["tos"] = cuadro_tos.instate(['selected'])
+    base_de_hechos["dolor_cabeza"] = cuadro_dolor_cabeza.instate(['selected'])
+    base_de_hechos["dificultad_respirar"] = cuadro_dificultad_respirar.instate(['selected'])
+    base_de_hechos["diarrea"] = cuadro_diarrea.instate(['selected'])
+    base_de_hechos["vomito"] = cuadro_vomito.instate(['selected'])
+    base_de_hechos["factores_riesgo"] = combo_factores_riesgo.get()
+    if "Anorexia" in combo_factores_riesgo.get():
         combo_factores_riesgo2.set("Ninguno")
-    if "Obesidad" in factores_seleccionados:
+    if "Obesidad" in combo_factores_riesgo.get():
         combo_factores_riesgo2.set("Ninguno")
-
-    # Se obtienen las adicciones seleccionadas
     if combo_adicciones.winfo_ismapped():
-        adicciones_seleccionadas = combo_adicciones.get()
-        base_de_hechos["adicciones"] = adicciones_seleccionadas
-    
-    # Se guarda la edad
+        base_de_hechos["adicciones"] = combo_adicciones.get()
     base_de_hechos["edad"] = int(combo_edad.get())
-
-    # Se imprime la base de hechos actualizada
     print(f"Hechos guardados en la base de hechos: {base_de_hechos}")
 
-# Base de Conocimientos
-def reglas_inferencia(hechos):
-    if "Obesidad" in hechos["factores_riesgo"] and hechos.get("fiebre") and hechos.get("dificultad_respirar"):
-        return "Posible diagnóstico: Neumonía"
-    elif "Anorexia" in hechos["factores_riesgo"] and hechos.get("dolor_cabeza") and hechos.get("vomito"):
-        return "Posible diagnóstico: Gastroenteritis"
-    elif hechos["edad"] >= 60 and hechos.get("dificultad_respirar"):
-        return "Posible diagnóstico: Neumonía en Adulto Mayor"
-    elif "Asma" in hechos["enfermedades"] and hechos.get("dificultad_respirar"):
-        return "Posible diagnóstico: Ataque de Asma"
-    elif hechos.get("fiebre") and hechos.get("tos"):
-        return "Posible diagnóstico: Gripe"
-    elif hechos.get("fiebre") and hechos.get("dolor_cabeza"):
-        return "Posible diagnóstico: Influenza"
-    elif hechos.get("fiebre") and hechos.get("dificultad_respirar"):
-        return "Posible diagnóstico: Neumonía"
-    elif hechos.get("diarrea") and hechos.get("vomito"):
-        return "Posible diagnóstico: Gastroenteritis"
-    elif hechos.get("fiebre") and hechos.get("congestion_nasal"):
-        return "Posible diagnóstico: Resfriado Común"
-    elif hechos.get("fiebre_alta") and hechos.get("dolor_muscular"):
-        return "Posible diagnóstico: Dengue"
-    elif hechos.get("dificultad_para_respirar") and hechos.get("sibilancias"):
-        return "Posible diagnóstico: Ataque de Asma"
-    elif hechos.get("fiebre") and hechos.get("dolor_abdominal"):
-        return "Posible diagnóstico: Infección Intestinal"
-    else:
-        return "No se puede determinar el diagnóstico"
+def mostrar_interfaz_usuario():
+    etiqueta_sintomas = ttk.Label(ventana, text="Síntomas:")
+    etiqueta_sintomas.pack()
 
-# Motor de Inferencia (Inference Engine)
+    etiqueta_sintomas = ttk.Label(ventana, text="")
+    etiqueta_sintomas.pack()
+
+    global cuadro_fiebre, cuadro_tos, cuadro_dolor_cabeza, cuadro_dificultad_respirar, cuadro_diarrea, cuadro_vomito
+    cuadro_fiebre = ttk.Checkbutton(ventana, text="Fiebre")
+    cuadro_fiebre.pack()
+
+    cuadro_tos = ttk.Checkbutton(ventana, text="Tos")
+    cuadro_tos.pack()
+
+    cuadro_dolor_cabeza = ttk.Checkbutton(ventana, text="Dolor de cabeza")
+    cuadro_dolor_cabeza.pack()
+
+    cuadro_dificultad_respirar = ttk.Checkbutton(ventana, text="Dificultad para respirar")
+    cuadro_dificultad_respirar.pack()
+
+    cuadro_diarrea = ttk.Checkbutton(ventana, text="Diarrea")
+    cuadro_diarrea.pack()
+
+    cuadro_vomito = ttk.Checkbutton(ventana, text="Vómito")
+    cuadro_vomito.pack()
+
+    etiqueta_edad = ttk.Label(ventana, text="Edad:")
+    etiqueta_edad.pack()
+
+    global combo_edad
+    combo_edad = ttk.Combobox(ventana, values=[f"{i}-{i+9} años" for i in range(10, 60, 10)], state="readonly")
+    combo_edad.pack()
+
+    etiqueta_factores_riesgo = ttk.Label(ventana, text="Factores de Riesgo:")
+    etiqueta_factores_riesgo.pack()
+
+    global combo_factores_riesgo, combo_factores_riesgo2
+    combo_factores_riesgo = ttk.Combobox(ventana, values=list(factores_riesgo.keys()), state="readonly")
+    combo_factores_riesgo.pack()
+
+    combo_factores_riesgo2 = ttk.Combobox(ventana, values=list(factores_riesgo.keys()), state="readonly")
+    combo_factores_riesgo2.pack()
+
+    etiqueta_adicciones = ttk.Label(ventana, text="Adicciones:")
+    etiqueta_adicciones.pack()
+
+    global combo_adicciones
+    combo_adicciones = ttk.Combobox(ventana, values=adicciones, state="readonly")
+    combo_adicciones.pack()
+
+    boton_guardar_nueva_enfermedad = ttk.Button(ventana, text="Agregar Nueva Enfermedad", command=abrir_ventana_agregar_enfermedad)
+    boton_guardar_nueva_enfermedad.pack()
+
+    boton_guardar_factores_riesgo = ttk.Button(ventana, text="Guardar sintomas", command=guardar_factores_riesgo)
+    boton_guardar_factores_riesgo.pack()
+
+    boton_consultar = ttk.Button(ventana, text="Consultar diagnóstico", command=inferir_diagnostico)
+    boton_consultar.pack()
+
+    cuadro_diagnostico = ttk.Label(ventana, text="Bienvenido al sistema experto médico")
+    cuadro_diagnostico.pack()
+
+def abrir_ventana_agregar_enfermedad():
+    ventana_agregar_enfermedad = tk.Toplevel(ventana)
+    ventana_agregar_enfermedad.title("Agregar Nueva Enfermedad")
+    ventana_agregar_enfermedad.config(width=400, height=300)
+
+    etiqueta_enfermedad = ttk.Label(ventana_agregar_enfermedad, text="Nombre de la Enfermedad:")
+    etiqueta_enfermedad.pack()
+
+    cuadro_enfermedad = ttk.Entry(ventana_agregar_enfermedad)
+    cuadro_enfermedad.pack()
+
+    etiqueta_sintomas = ttk.Label(ventana_agregar_enfermedad, text="Síntomas Relacionados:")
+    etiqueta_sintomas.pack()
+
+    global cuadro_fiebre, cuadro_tos, cuadro_dolor_cabeza, cuadro_dificultad_respirar, cuadro_diarrea, cuadro_vomito
+    cuadro_fiebre = ttk.Checkbutton(ventana_agregar_enfermedad, text="Fiebre")
+    cuadro_fiebre.pack()
+
+    cuadro_tos = ttk.Checkbutton(ventana_agregar_enfermedad, text="Tos")
+    cuadro_tos.pack()
+
+    cuadro_dolor_cabeza = ttk.Checkbutton(ventana_agregar_enfermedad, text="Dolor de cabeza")
+    cuadro_dolor_cabeza.pack()
+
+    cuadro_dificultad_respirar = ttk.Checkbutton(ventana_agregar_enfermedad, text="Dificultad para respirar")
+    cuadro_dificultad_respirar.pack()
+
+    cuadro_diarrea = ttk.Checkbutton(ventana_agregar_enfermedad, text="Diarrea")
+    cuadro_diarrea.pack()
+
+    cuadro_vomito = ttk.Checkbutton(ventana_agregar_enfermedad, text="Vómito")
+    cuadro_vomito.pack()
+
+    boton_guardar_enfermedad = ttk.Button(ventana_agregar_enfermedad, text="Guardar Enfermedad", command=guardar_nueva_enfermedad)
+    boton_guardar_enfermedad.pack()
+
+def guardar_nueva_enfermedad():
+    # Aquí puedes guardar la nueva enfermedad y los síntomas relacionados en tu base de conocimientos.
+    pass
+
+def reglas_inferencia(hechos):
+    # Agrega aquí tus reglas de inferencia
+    pass
+
 def inferir_diagnostico():
+    guardar_factores_riesgo()  # Guarda la base de hechos antes de inferir el diagnóstico
     hechos = {
         "fiebre": base_de_hechos.get("fiebre", False),
         "tos": base_de_hechos.get("tos", False),
@@ -88,91 +159,13 @@ def inferir_diagnostico():
         "adicciones": base_de_hechos.get("adicciones", []),
         "edad": base_de_hechos.get("edad", 0)
     }
-
     diagnostico = reglas_inferencia(hechos)
     cuadro_diagnostico.config(text=f"El diagnóstico es: {diagnostico}")
-
-def consultar_diagnostico():
-    inferir_diagnostico()
 
 ventana = tk.Tk()
 ventana.title("Sistema experto médico para la detección de enfermedades")
 ventana.config(width=600, height=600)
 
-# Interfaz de Usuario
-
-etiqueta_sintomas = ttk.Label(ventana, text="Síntomas:")
-etiqueta_sintomas.pack()
-
-etiqueta_sintomas = ttk.Label(ventana, text="")
-etiqueta_sintomas.pack()
-
-etiqueta_edad = ttk.Label(ventana, text="Edad:")
-etiqueta_edad.pack()
-
-combo_edad = ttk.Combobox(ventana, values=list(range(10, 70)), state="readonly")
-combo_edad.pack()
-
-etiqueta_sintomas = ttk.Label(ventana, text="")
-etiqueta_sintomas.pack()
-
-cuadro_fiebre = ttk.Entry(ventana)
-cuadro_fiebre.pack()
-etiqueta_fiebre = ttk.Label(ventana, text="Fiebre (si/no):")
-etiqueta_fiebre.pack()
-
-cuadro_tos = ttk.Entry(ventana)
-cuadro_tos.pack()
-etiqueta_tos = ttk.Label(ventana, text="Tos (si/no):")
-etiqueta_tos.pack()
-
-cuadro_dolor_cabeza = ttk.Entry(ventana)
-cuadro_dolor_cabeza.pack()
-etiqueta_dolor_cabeza = ttk.Label(ventana, text="Dolor de cabeza (si/no):")
-etiqueta_dolor_cabeza.pack()
-
-cuadro_dificultad_respirar = ttk.Entry(ventana)
-cuadro_dificultad_respirar.pack()
-etiqueta_dificultad_respirar = ttk.Label(ventana, text="Dificultad para respirar (si/no):")
-etiqueta_dificultad_respirar.pack()
-
-cuadro_diarrea = ttk.Entry(ventana)
-cuadro_diarrea.pack()
-etiqueta_diarrea = ttk.Label(ventana, text="Diarrea (si/no):")
-etiqueta_diarrea.pack()
-
-cuadro_vomito = ttk.Entry(ventana)
-cuadro_vomito.pack()
-etiqueta_vomito = ttk.Label(ventana, text="Vómito (si/no):")
-etiqueta_vomito.pack()
-
-# Combo Box para Factores de Riesgo
-etiqueta_factores_riesgo = ttk.Label(ventana, text="Factores de Riesgo:")
-etiqueta_factores_riesgo.pack()
-
-combo_factores_riesgo = ttk.Combobox(ventana, values=list(factores_riesgo.keys()), state="readonly")
-combo_factores_riesgo.pack()
-
-combo_factores_riesgo2 = ttk.Combobox(ventana, values=list(factores_riesgo.keys()), state="readonly")
-combo_factores_riesgo2.pack()
-
-# Combo Box para Adicciones
-etiqueta_adicciones = ttk.Label(ventana, text="Adicciones:")
-etiqueta_adicciones.pack()
-
-# Combo box de adicciones
-combo_adicciones = ttk.Combobox(ventana, values=adicciones, state="readonly")
-combo_adicciones.pack()
-
-# Botón para Guardar Factores de Riesgo
-boton_guardar_factores_riesgo = ttk.Button(ventana, text="Guardar sintomas", command=guardar_factores_riesgo)
-boton_guardar_factores_riesgo.pack()
-
-# Botón para Consultar Diagnóstico
-boton_consultar = ttk.Button(ventana, text="Consultar diagnóstico", command=consultar_diagnostico)
-boton_consultar.pack()
-
-cuadro_diagnostico = ttk.Label(ventana, text="Bienvenido al sistema experto médico")
-cuadro_diagnostico.pack()
+mostrar_interfaz_usuario()
 
 ventana.mainloop()
--- se copia.py
+++ se copia.py
@@ -4,7 +4,49 @@ from tkinter import ttk
 # Base de Hechos (Fact Base)
 base_de_hechos = {}
 
-# Base de Conocimientos (Knowledge Base)
+# Lista de Factores de Riesgo con Síntomas
+factores_riesgo = {
+    "Ninguno":"",
+    "Obesidad": "Aumento de peso, Dificultad para moverse",
+    "Anorexia": "Pérdida de peso, Debilidad"
+    
+}
+
+# Lista de Adicciones
+adicciones = ["Ninguno", "Alcoholismo", "Tabaquismo", "Drogadicción"]
+
+# Función para guardar factores de riesgo y otros datos en la base de hechos
+def guardar_factores_riesgo():
+    # Se guarda cada síntoma y su valor en la base de hechos
+    base_de_hechos["fiebre"] = cuadro_fiebre.get().lower() == "si"
+    base_de_hechos["tos"] = cuadro_tos.get().lower() == "si"
+    base_de_hechos["dolor_cabeza"] = cuadro_dolor_cabeza.get().lower() == "si"
+    base_de_hechos["dificultad_respirar"] = cuadro_dificultad_respirar.get().lower() == "si"
+    base_de_hechos["diarrea"] = cuadro_diarrea.get().lower() == "si"
+    base_de_hechos["vomito"] = cuadro_vomito.get().lower() == "si"
+    
+    # Se obtienen los factores de riesgo seleccionados
+    factores_seleccionados = combo_factores_riesgo.get()
+    base_de_hechos["factores_riesgo"] = factores_seleccionados
+    
+    # Se deshabilita Anorexia y Obesidad en el otro combo box
+    if "Anorexia" in factores_seleccionados:
+        combo_factores_riesgo2.set("Ninguno")
+    if "Obesidad" in factores_seleccionados:
+        combo_factores_riesgo2.set("Ninguno")
+
+    # Se obtienen las adicciones seleccionadas
+    if combo_adicciones.winfo_ismapped():
+        adicciones_seleccionadas = combo_adicciones.get()
+        base_de_hechos["adicciones"] = adicciones_seleccionadas
+    
+    # Se guarda la edad
+    base_de_hechos["edad"] = int(combo_edad.get())
+
+    # Se imprime la base de hechos actualizada
+    print(f"Hechos guardados en la base de hechos: {base_de_hechos}")
+
+# Base de Conocimientos
 def reglas_inferencia(hechos):
     if "Obesidad" in hechos["factores_riesgo"] and hechos.get("fiebre") and hechos.get("dificultad_respirar"):
         return "Posible diagnóstico: Neumonía"
@@ -33,28 +75,45 @@ def reglas_inferencia(hechos):
     else:
         return "No se puede determinar el diagnóstico"
 
-
 # Motor de Inferencia (Inference Engine)
 def inferir_diagnostico():
-    diagnostico = reglas_inferencia(base_de_hechos)
-    return diagnostico
+    hechos = {
+        "fiebre": base_de_hechos.get("fiebre", False),
+        "tos": base_de_hechos.get("tos", False),
+        "dolor_cabeza": base_de_hechos.get("dolor_cabeza", False),
+        "dificultad_respirar": base_de_hechos.get("dificultad_respirar", False),
+        "diarrea": base_de_hechos.get("diarrea", False),
+        "vomito": base_de_hechos.get("vomito", False),
+        "factores_riesgo": base_de_hechos.get("factores_riesgo", []),
+        "adicciones": base_de_hechos.get("adicciones", []),
+        "edad": base_de_hechos.get("edad", 0)
+    }
 
-def guardar_hechos(hechos):
-    base_de_hechos.update(hechos)
-    print(f"Hechos actualizados: {base_de_hechos}")
+    diagnostico = reglas_inferencia(hechos)
+    cuadro_diagnostico.config(text=f"El diagnóstico es: {diagnostico}")
 
 def consultar_diagnostico():
-    diagnostico = inferir_diagnostico()
-    if diagnostico is None:
-        diagnostico = "No se puede determinar el diagnóstico"
-    cuadro_diagnostico.config(text=f"El diagnóstico es: {diagnostico}")
+    inferir_diagnostico()
 
 ventana = tk.Tk()
 ventana.title("Sistema experto médico para la detección de enfermedades")
-ventana.config(width=500, height=400)
+ventana.config(width=600, height=600)
 
 # Interfaz de Usuario
+
 etiqueta_sintomas = ttk.Label(ventana, text="Síntomas:")
+etiqueta_sintomas.pack()
+
+etiqueta_sintomas = ttk.Label(ventana, text="")
+etiqueta_sintomas.pack()
+
+etiqueta_edad = ttk.Label(ventana, text="Edad:")
+etiqueta_edad.pack()
+
+combo_edad = ttk.Combobox(ventana, values=list(range(10, 70)), state="readonly")
+combo_edad.pack()
+
+etiqueta_sintomas = ttk.Label(ventana, text="")
 etiqueta_sintomas.pack()
 
 cuadro_fiebre = ttk.Entry(ventana)
@@ -87,16 +146,29 @@ cuadro_vomito.pack()
 etiqueta_vomito = ttk.Label(ventana, text="Vómito (si/no):")
 etiqueta_vomito.pack()
 
-boton_guardar = ttk.Button(ventana, text="Guardar en la base de hechos", command=lambda: guardar_hechos({
-    "fiebre": cuadro_fiebre.get().lower() == "si",
-    "tos": cuadro_tos.get().lower() == "si",
-    "dolor_cabeza": cuadro_dolor_cabeza.get().lower() == "si",
-    "dificultad_respirar": cuadro_dificultad_respirar.get().lower() == "si",
-    "diarrea": cuadro_diarrea.get().lower() == "si",
-    "vomito": cuadro_vomito.get().lower() == "si",
-}))
-boton_guardar.pack()
+# Combo Box para Factores de Riesgo
+etiqueta_factores_riesgo = ttk.Label(ventana, text="Factores de Riesgo:")
+etiqueta_factores_riesgo.pack()
 
+combo_factores_riesgo = ttk.Combobox(ventana, values=list(factores_riesgo.keys()), state="readonly")
+combo_factores_riesgo.pack()
+
+combo_factores_riesgo2 = ttk.Combobox(ventana, values=list(factores_riesgo.keys()), state="readonly")
+combo_factores_riesgo2.pack()
+
+# Combo Box para Adicciones
+etiqueta_adicciones = ttk.Label(ventana, text="Adicciones:")
+etiqueta_adicciones.pack()
+
+# Combo box de adicciones
+combo_adicciones = ttk.Combobox(ventana, values=adicciones, state="readonly")
+combo_adicciones.pack()
+
+# Botón para Guardar Factores de Riesgo
+boton_guardar_factores_riesgo = ttk.Button(ventana, text="Guardar sintomas", command=guardar_factores_riesgo)
+boton_guardar_factores_riesgo.pack()
+
+# Botón para Consultar Diagnóstico
 boton_consultar = ttk.Button(ventana, text="Consultar diagnóstico", command=consultar_diagnostico)
 boton_consultar.pack()
 
